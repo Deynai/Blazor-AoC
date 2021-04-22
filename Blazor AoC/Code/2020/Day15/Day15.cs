d57a519a816e4463de95966edf1fab2e3feeb48e
Rowan <rowan.p@hotmail.co.uk>
2021-04-22 19:05:22 +0100

    day 14 & 15

--- Blazor AoC/Code/2020/Day15/Day15.cs
+++ Blazor AoC/Code/2020/Day15/Day15.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 
 namespace Blazor_AoC.Code._2020
 {
-    [Puzzle(15, "")]
+    [Puzzle(15, "Rambunctious Recitation")]
     public class Day15 : Solution
     {
         private string inputString = string.Empty;
+        private uint[] history = new uint[30_000_000];
+        private uint count = 1;
+        private uint num;
 
         public Day15(string inputBox)
         {
@@ -17,12 +20,40 @@ namespace Blazor_AoC.Code._2020
 
         public override string GetPart1()
         {
-            return base.GetPart1();
+            uint[] start_sequence = inputString.Split(",").Select(n => uint.Parse(n)).ToArray();
+            num = start_sequence.Last();
+
+            for(int i = 0; i < start_sequence.Length-1; i++)
+            {
+                history[start_sequence[i]] = count;
+                count++;
+            }
+
+            while(count < 2020)
+            {
+                GetNextTerm();
+                count++;
+            }
+
+            return num.ToString();
         }
 
         public override string GetPart2()
         {
-            return base.GetPart2();
+            while(count < 30_000_000)
+            {
+                GetNextTerm();
+                count++;
+            }
+
+            return num.ToString();
+        }
+
+        private void GetNextTerm()
+        {
+            uint next = history[num].Equals(0) ? 0 : count - history[num];
+            history[num] = count;
+            num = next;
         }
     }
 }
